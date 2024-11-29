a236bd414f8315d6f16d6306ceed9fbfd921cdc1
WOJCIJ10 <jaroslaw.wojciechowski2@kingfisher.com>
2024-11-29 16:04:31 +0100

    parse_file added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod utils;
+
+use utils::read_lines;
+
+const FILE_NAME: &str = "src/inputs/input-example.txt";
+
 fn main() {
-    println!("Hello, world!");
+    parse_file(FILE_NAME);
+}
+
+fn parse_file(file_name: &str) {
+    if let Ok(lines) = read_lines(file_name) {
+        for line in lines.flatten() {
+            println!("{line}");
+        }
+    }
 }
