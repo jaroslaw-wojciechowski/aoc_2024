e278572754bf90048775e3329928b096f3eecfa1
jaroslaw-wojciechowski <>
2024-12-01 14:13:04 +0100

    day 1 part 2

--- src/solutions/01-1.rs
+++ src/solutions/01-1.rs
@@ -13,18 +13,23 @@ fn main() {
 }
 
 fn parse_file(file_name: &str) -> (Vec<i32>, Vec<i32>) {
-    let mut left: Vec<i32> = Vec::new();
-    let mut right: Vec<i32> = Vec::new();
+    let mut left_vec: Vec<i32> = Vec::new();
+    let mut right_vec: Vec<i32> = Vec::new();
 
     if let Ok(lines) = read_lines(file_name) {
         for line in lines.flatten() {
-            let input: (&str, &str) = line.split_once("   ").unwrap();
-            left.push(input.0.parse().unwrap());
-            right.push(input.1.parse().unwrap());
+            let (left, right) = line
+                .split_once("   ")
+                .expect("File should contain '   ' separator");
+
+            let left_num = left.parse().expect("Left value should be a number");
+            let right_num = right.parse().expect("Right value should be a number");
+            left_vec.push(left_num);
+            right_vec.push(right_num);
         }
     }
 
-    return (left, right);
+    return (left_vec, right_vec);
 }
 
 fn calculate_result((mut left, mut right): (Vec<i32>, Vec<i32>)) -> u32 {
