ecd3dd0de138c9ae999eefffafb60aa4ca931811
jaroslaw-wojciechowski <>
2024-12-08 15:18:42 +0100

    day 8 part 2 example

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,17 @@ const FILE_NAME: &str = "src/inputs/input-example.txt";
 // const FILE_NAME: &str = "src/inputs/08-1.txt";
 
 fn main() {
-    let mut matrix = write_matrix();
+    let (mut matrix, init_count) = write_matrix();
     let coords_map = build_coords(&mut matrix);
-    count_hash(coords_map, &mut matrix);
+    count_hash(coords_map, &mut matrix, init_count);
     print_matrix(matrix);
 }
 
-fn count_hash(coords_map: HashMap<char, Vec<(i32, i32)>>, matrix: &mut [[char; SIZE]; SIZE]) {
+fn count_hash(
+    coords_map: HashMap<char, Vec<(i32, i32)>>,
+    matrix: &mut [[char; SIZE]; SIZE],
+    init_count: i32,
+) {
     let mut counter: i32 = 0;
     let mut overlap_vec: Vec<(i32, i32)> = Vec::new();
     for vec in coords_map.values().into_iter() {
@@ -25,40 +29,57 @@ fn count_hash(coords_map: HashMap<char, Vec<(i32, i32)>>, matrix: &mut [[char; S
                 let x_diff = (vec[i].0 - vec[j].0).abs();
                 let y_diff = (vec[i].1 - vec[j].1).abs();
 
-                let an1_x = match (vec[i].0, vec[j].0) {
-                    (x1, x2) if x1 > x2 => x1 + x_diff,
-                    (x1, x2) if x1 < x2 => x1 - x_diff,
-                    (x1, x2) if x1 == x2 => x1,
-                    _ => panic!("something wend wrong"),
-                };
+                let mut vec_i = vec[i];
+                let mut vec_j = vec[j];
 
-                let an1_y = match (vec[i].1, vec[j].1) {
-                    (y1, y2) if y1 > y2 => y1 + y_diff,
-                    (y1, y2) if y1 < y2 => y1 - y_diff,
-                    (y1, y2) if y1 == y2 => y1,
-                    _ => panic!("something wend wrong"),
-                };
+                'inner: loop {
+                    let an1_x = match (vec_i.0, vec_j.0) {
+                        (x1, x2) if x1 > x2 => x1 + x_diff,
+                        (x1, x2) if x1 < x2 => x1 - x_diff,
+                        (x1, x2) if x1 == x2 => x1,
+                        _ => panic!("something wend wrong"),
+                    };
 
-                let an2_x = match (vec[i].0, vec[j].0) {
-                    (x1, x2) if x2 > x1 => x2 + x_diff,
-                    (x1, x2) if x2 < x1 => x2 - x_diff,
-                    (x1, x2) if x2 == x1 => x2,
-                    _ => panic!("something wend wrong"),
-                };
+                    let an1_y = match (vec_i.1, vec_j.1) {
+                        (y1, y2) if y1 > y2 => y1 + y_diff,
+                        (y1, y2) if y1 < y2 => y1 - y_diff,
+                        (y1, y2) if y1 == y2 => y1,
+                        _ => panic!("something wend wrong"),
+                    };
 
-                let an2_y = match (vec[i].1, vec[j].1) {
-                    (y1, y2) if y2 > y1 => y2 + y_diff,
-                    (y1, y2) if y2 < y1 => y2 - y_diff,
-                    (y1, y2) if y2 == y1 => y2,
-                    _ => panic!("something wend wrong"),
-                };
+                    let an2_x = match (vec_i.0, vec_j.0) {
+                        (x1, x2) if x2 > x1 => x2 + x_diff,
+                        (x1, x2) if x2 < x1 => x2 - x_diff,
+                        (x1, x2) if x2 == x1 => x2,
+                        _ => panic!("something wend wrong"),
+                    };
 
-                put_antinode(an1_x, an1_y, matrix, &mut counter, &mut overlap_vec);
-                put_antinode(an2_x, an2_y, matrix, &mut counter, &mut overlap_vec);
+                    let an2_y = match (vec_i.1, vec_j.1) {
+                        (y1, y2) if y2 > y1 => y2 + y_diff,
+                        (y1, y2) if y2 < y1 => y2 - y_diff,
+                        (y1, y2) if y2 == y1 => y2,
+                        _ => panic!("something wend wrong"),
+                    };
+
+                    if !put_antinode(an2_x, an2_y, matrix, &mut counter, &mut overlap_vec)
+                        & !put_antinode(an1_x, an1_y, matrix, &mut counter, &mut overlap_vec)
+                    {
+                        break 'inner;
+                    }
+
+                    vec_i = (an1_x, an1_y);
+                    vec_j = (an2_x, an2_y);
+                }
             }
         }
     }
+
+    let overlapped_counter = overlap_vec.len() as i32;
     println!("counter: {}", counter);
+    println!("init_counter: {}", init_count);
+    println!("overlapped: {}", overlapped_counter);
+
+    println!("result: {}", counter + init_count - overlapped_counter);
 }
 
 fn put_antinode(
@@ -76,16 +97,13 @@ fn put_antinode(
                 matrix[x as usize][y as usize] = '#';
                 *counter = *counter + 1;
             }
-            '#' => println!("overlap #"),
+            '#' => {
+                println!("DONT FOR overlap #");
+            }
             _ => {
-                println!("overlap other");
-                if overlap_vec.contains(&(x, y)) {
-                    println!("2nd overlap other");
-                } else {
-                    println!("1nd overlap other");
-                    *counter = *counter + 1;
-                    overlap_vec.push((x, y));
-                }
+                println!("DO   FOR overlap ANTENNA");
+                overlap_vec.push((x, y));
+                *counter = *counter + 1;
             }
         }
         return true;
@@ -112,27 +130,28 @@ fn build_coords(matrix: &mut [[char; SIZE]; SIZE]) -> HashMap<char, Vec<(i32, i3
     return coords_map;
 }
 
-fn write_matrix() -> [[char; SIZE]; SIZE] {
+fn write_matrix() -> ([[char; SIZE]; SIZE], i32) {
     let mut matrix: [[char; SIZE]; SIZE] = [[' '; SIZE]; SIZE];
+    let mut init_count = 0;
 
     let file_name = FILE_NAME;
     if let Ok(lines) = read_lines(file_name) {
         for (y, line) in lines.flatten().enumerate() {
             for (x, char) in line.chars().enumerate() {
                 matrix[x][y] = char;
+                if char != '.' {
+                    init_count += 1;
+                }
             }
         }
     }
-    return matrix;
+
+    return (matrix, init_count);
 }
 
 fn print_matrix(matrix: [[char; SIZE]; SIZE]) {
-    let mut counter = 0;
     for y in 0..SIZE {
         for x in 0..SIZE {
-            if matrix[x][y] == '#' {
-                counter += 1;
-            }
             print!("{}", matrix[x][y]);
         }
         println!();
