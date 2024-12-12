7a0f46ec3ca8c79a56a0ab8aba6e243176d9939a
jaroslaw-wojciechowski <>
2024-12-12 20:05:42 +0100

    day 12 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,94 @@
-use std::collections::HashMap;
+mod utils;
+
+use utils::read_lines;
+
+// const SIZE: usize = 10;
+const SIZE: usize = 140;
+// const FILE_NAME: &str = "src/inputs/input-example.txt";
+const FILE_NAME: &str = "src/inputs/12-1.txt";
 
 fn main() {
-    let input = "475449 2599064 213 0 2 65 5755 51149";
-    // let input = String::from("125 17");
-    let mut map: HashMap<String, i64> = HashMap::new();
-    for i in input.split(" ").into_iter() {
-        if map.contains_key(&String::from(i)) {
-            let val = map.get(&String::from(i)).unwrap();
-            map.insert(String::from(i), val + 1);
+    let matrix = write_matrix();
+    traverse_matrix(&matrix);
+}
+
+fn write_matrix() -> [[char; SIZE]; SIZE] {
+    let mut matrix: [[char; SIZE]; SIZE] = [[' '; SIZE]; SIZE];
+
+    let file_name = FILE_NAME;
+    if let Ok(lines) = read_lines(file_name) {
+        for (y, line) in lines.flatten().enumerate() {
+            for (x, char) in line.chars().enumerate() {
+                matrix[x][y] = char;
+            }
+        }
+    }
+    return matrix;
+}
+
+fn traverse_matrix(matrix: &[[char; SIZE]; SIZE]) {
+    let mut visited: Vec<(usize, usize)> = Vec::new();
+    let mut diff = 0;
+    let mut result = 0;
+
+    for y in 0..SIZE {
+        for x in 0..SIZE {
+            if !visited.contains(&(x, y)) {
+                let wynik = bfs(&matrix, &mut visited, x, y);
+                result += (visited.len() - diff) * wynik as usize;
+                println!("visited: {} * wynik: {}", visited.len() - diff, wynik);
+                diff = visited.len();
+            }
+        }
+    }
+    println!("result {}", result);
+}
+
+fn bfs(
+    matrix: &[[char; SIZE]; SIZE],
+    visited: &mut Vec<(usize, usize)>,
+    x: usize,
+    y: usize,
+) -> i32 {
+    let mut fields: Vec<(usize, usize)> = Vec::new();
+    let mut count: i32 = 0;
+
+    if visited.contains(&(x, y)) {
+        return 0;
+    }
+    visited.push((x, y));
+
+    if y > 0 {
+        fields.push((x, y - 1));
+    } else {
+        count += 1;
+    }
+
+    if y < SIZE - 1 {
+        fields.push((x, y + 1));
+    } else {
+        count += 1;
+    }
+
+    if x > 0 {
+        fields.push((x - 1, y));
+    } else {
+        count += 1;
+    }
+
+    if x < SIZE - 1 {
+        fields.push((x + 1, y));
+    } else {
+        count += 1;
+    }
+
+    for coords in fields {
+        if matrix[coords.0][coords.1] != matrix[x][y] {
+            count += 1;
         } else {
-            map.insert(String::from(i), 1);
+            count += bfs(matrix, visited, coords.0, coords.1);
         }
     }
 
-    println!("init map: {:?}", map);
-
-    for i in 0..75 {
-        map = process_input(&map);
-        let sum: i64 = map.values().map(|&x| x as i64).sum();
-        // println!("iter: {} map len: {:?}; map: {:?}", i + 1, sum, map);
-        println!("iter: {} map len: {:?}", i + 1, sum);
-    }
-}
-
-fn process_input(in_map: &HashMap<String, i64>) -> HashMap<String, i64> {
-    let mut out_map: HashMap<String, i64> = HashMap::with_capacity(in_map.len() * 2);
-
-    for (key, &val) in in_map {
-        match key.as_str() {
-            "0" => {
-                *out_map.entry(String::from("1")).or_insert(0) += val;
-            }
-            "1" => {
-                *out_map.entry(String::from("2024")).or_insert(0) += val;
-            }
-            "20" => {
-                *out_map.entry(String::from("2")).or_insert(0) += val;
-                *out_map.entry(String::from("0")).or_insert(0) += val;
-            }
-            "24" => {
-                *out_map.entry(String::from("2")).or_insert(0) += val;
-                *out_map.entry(String::from("4")).or_insert(0) += val;
-            }
-            "2024" => {
-                *out_map.entry(String::from("20")).or_insert(0) += val;
-                *out_map.entry(String::from("24")).or_insert(0) += val;
-            }
-            key if key.len() % 2 == 0 => {
-                let (l, r) = split_stone(key.to_string());
-                *out_map.entry(l).or_insert(0) += val;
-                *out_map.entry(r).or_insert(0) += val;
-            }
-            key => {
-                let mul_op = mul_stone(key.to_string());
-                *out_map.entry(mul_op).or_insert(0) += val;
-            }
-        }
-    }
-
-    out_map
-}
-
-fn mul_stone(s: String) -> String {
-    let big_num: i64 = s.parse().unwrap();
-    let sum: i64 = big_num * 2024;
-    return sum.to_string();
-}
-
-fn split_stone(s: String) -> (String, String) {
-    let (left, mut right) = s.split_at(s.len() / 2);
-
-    right = right.trim_start_matches('0');
-    if right.len() == 0 {
-        right = "0";
-    }
-    return (String::from(left), String::from(right));
+    return count;
 }
